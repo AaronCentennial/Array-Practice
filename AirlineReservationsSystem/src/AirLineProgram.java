785a5660fcbadb2bd869bad85261c491e891741a
aagavin <aferna95@my.centennialcollege.ca>
2015-11-07 21:51:53 -0500

    added native look and feel

--- AirlineReservationsSystem/src/AirLineProgram.java
+++ AirlineReservationsSystem/src/AirLineProgram.java
@@ -1,10 +1,35 @@
-
+import javax.swing.*;
 
 /**
  * Created by Aaron Fernandes(300773526) on November 2015.
  */
 public class AirLineProgram {
 	public static void main(String[] args){
+
+		for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+			if ("GTK+".equals(info.getName()) || "windows".equals(info.getName()) || "substance".equals(info.getName())) {
+				try {
+					UIManager.setLookAndFeel(info.getClassName());
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				} catch (InstantiationException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				} catch (UnsupportedLookAndFeelException e) {
+					e.printStackTrace();
+				}
+				break;
+			}
+		}
+
+/*
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
+
 		AirLine air = new AirLine();
 		AirLineGui gui = new AirLineGui(air);
 	}
--- DiceRollingApp/src/Program.java
+++ DiceRollingApp/src/Program.java
@@ -1,9 +1,3 @@
-import com.sun.java.swing.plaf.gtk.GTKLookAndFeel;
-
-import java.util.ArrayList;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-
 /**
  * Created by Aaron Fernandes(300773526) on November 2015.
  */
